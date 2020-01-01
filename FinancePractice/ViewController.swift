057a6adfc2f30f904418fe37b60d88d8879d2e1a
karthik.ms <karthik.ms@zohocorp.com>
2020-01-01 12:57:18 +0530

    Initial Commit

---
+++ FinancePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinancePractice
+//
+//  Created by Karthik M S on 01/01/20.
+//  Copyright © 2020 Zoho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ FinancePracticeTests/FinancePracticeTests.swift
@@ -0,0 +1,34 @@
+//
+//  FinancePracticeTests.swift
+//  FinancePracticeTests
+//
+//  Created by Karthik M S on 01/01/20.
+//  Copyright © 2020 Zoho. All rights reserved.
+//
+
+import XCTest
+@testable import FinancePractice
+
+class FinancePracticeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
